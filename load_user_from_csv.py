f2066d1c5d8ece2aebcb1c128b70cd019d1efb64
thheie <thh@computas.com>
2023-05-08 12:14:36 +0200

    Added doc stings

--- load_user_from_csv.py
+++ load_user_from_csv.py
@@ -1,3 +1,8 @@
+"""
+Loads and iterates though a list  of Atlassian user ids and a new email address for that specific user.
+The emails are updated using the Atlassian user api for email updates
+see https://developer.atlassian.com/cloud/admin/user-management/rest/api-group-email/#api-users-account-id-manage-email-put for details
+"""
 import csv
 import requests
 import json
@@ -5,8 +10,12 @@ import os.path
 from logger import logger
 
 
-def load_csv_file():
-    filename = 'userlist.csv'
+def load_csv_file(filename):
+    """
+    Loads user identifier and new email address from a csv file
+    :param filename: The name of the file that is to be loaded
+    :return: A list of dicts. The identifier of the dicts is the same as the column headers in the CSV file
+    """
     logger.info(f'loading csv file: {filename}')
     value_list = []
     if os.path.isfile(filename):
@@ -20,12 +29,22 @@ def load_csv_file():
 
 
 def set_url(item):
+    """
+    Creates the urs that is cals the api
+    :param item: a dict that contains the user id
+    :return: the complete URL for the api
+    """
     url = f'https://api.atlassian.com/users/{item["User id"]}/manage/email'
     # url = f'https://api.atlassian.com/users/{item["Atlassian ID"]}/manage/api-tokens'
     return url
 
 
 def update_user_email(user_list):
+    """
+    updates user emails using the Atlassian rest api for email updates.
+    :param user_list: A list of dicts that must contain a value for user_id and a value for the new email address.
+    :return: Nothing
+    """
     for user_id in user_list:
         url = set_url(user_id)
         headers = {
@@ -44,8 +63,10 @@ def update_user_email(user_list):
             logger.info(f'Unable to change email address for user {user_id["email"]} to {user_id["email - new"]}. Error code: {response.status_code}')
             logger.info(f'{response.text}')
 
+
 def main():
-    user_list = load_csv_file()
+    filename = 'userlist.csv'
+    user_list = load_csv_file(filename)
     if len(user_list) > 0:
         update_user_email(user_list)
 
